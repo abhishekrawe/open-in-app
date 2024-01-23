7e556bf8b7da2482c9dba27e3aa45e51c11eab16
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-24 01:45:29 +0530

    created individual SVG component files for each icon

---
+++ src/assets/icons/LebelRemoveIcon.jsx
@@ -0,0 +1,7 @@
+export function LebelRemoveIcon() {
+  return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+          <path d="M5 5L8 8M8 8L5 11M8 8L11 11M8 8L11 5" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+  )
+}
