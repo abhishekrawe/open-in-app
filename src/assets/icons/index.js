b6093a3428109292c500de384751509c5d7d88ef
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-24 01:46:15 +0530

    central index file for svg for easier import throughout application

---
+++ src/assets/icons/index.js
@@ -0,0 +1,27 @@
+import { BaseLogoIcon } from "./BaseLogoIcon";
+import { BellIcon } from "./BellIcon";
+import { CalenderIcon } from "./CalenderIcon";
+import { ControlsIcon } from "./ControlsIcon";
+import { DashboardIcon } from "./DashboardIcon";
+import { ExcelIcon } from "./ExcelIcon";
+import { InvoiceIcon } from "./InvoiceIcon";
+import { LebelRemoveIcon } from "./LebelRemoveIcon";
+import { NotificationIcon } from "./NotificationIcon";
+import { ScheduleIcon } from "./ScheduleIcon";
+import { SettingsIcon } from "./SettingsIcon";
+import { UploadIcon } from "./UploadIcon";
+
+export {
+  BaseLogoIcon,
+  BellIcon,
+  CalenderIcon,
+  ControlsIcon,
+  DashboardIcon,
+  ExcelIcon,
+  InvoiceIcon,
+  LebelRemoveIcon,
+  NotificationIcon,
+  ScheduleIcon,
+  SettingsIcon,
+  UploadIcon,
+};
