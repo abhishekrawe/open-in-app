cd7f1578a424cb86a9baad0fcb6df26f61716787
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-25 01:24:18 +0530

    Functionlity of Add Tags and Selcted Tags Badge

--- src/components/DataUploads.jsx
+++ src/components/DataUploads.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React , {useState} from "react";
 import { sampledata } from '../data/sample-data';
 import TagSlector from "../Popover/TagSlector";
-import { ChevronDownIcon } from '../assets/icons'
+import { ChevronDownIcon, LebelRemoveIcon } from '../assets/icons'
 
 function DataUploads() {
+  const [selectedTagsMap, setSelectedTagsMap] = useState({});
 
+  const tags = [
+    { value: 'Tag 1', label: 'Tag 1' },
+    { value: 'Tag 2', label: 'Tag 2' },
+    { value: 'Tag 3', label: 'Tag 3' },
+    { value: 'Tag 4', label: 'Tag 4' },
+    { value: 'Tag 5', label: 'Tag 5' },
+  ];
+
+  const handleTagChange = (event, dataId) => {
+    const selectedTagValue = event.target.value;
+    setSelectedTagsMap((prevMap) => ({
+      ...prevMap,
+      [dataId]: prevMap[dataId] ? [...prevMap[dataId], selectedTagValue] : [selectedTagValue],
+    }));
+  };
   
   return (
     <div className='rounded-3xl'>
       <div className='hidden md:flex items-center py-5 '>
-        <span className='text-black-12 text-xl font-medium'>Upload CSV</span>
+        <span className='text-black-12 text-xl font-medium'>Uploads</span>
       </div>
       <table className="w-full text-sm text-left bg-white-30 rounded-lg p">
         <thead className="text-md">
@@ -32,7 +48,7 @@ function DataUploads() {
           </tr>
         </thead>
         {sampledata.map((data) =>
-          <tbody>
+          <tbody key={data.id}>
             <tr className="bg-white-20 rounded-md text-base font-figtree">
               <th scope="row" className="px-6 py-4 font-medium text-gray-lightbulma  whitespace-nowrap dark:text-white">
                 {data.id}
@@ -44,10 +60,21 @@ function DataUploads() {
                 {data.prefix}
               </td>
               <td className="px-6 py-4 font-figtree text-gray-lightbulma ">
-                <TagSlector />
+                <TagSlector
+                  tags={tags}
+                  value={selectedTagsMap[data.id] || 'default'}
+                  onChange={(event) => handleTagChange(event, data.id)}
+                />
               </td>
-              <td className="px-6 py-4 font-figtree text-gray-lightbulma ">
-                Selected Tags
+              <td className="px-6  flex py-4 font-figtree text-gray-lightbulma ">
+                {selectedTagsMap[data.id] &&
+                  selectedTagsMap[data.id].map((tag, index) => (
+                    <span key={index} className="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-white-10 bg-blue-10 rounded dark:text-white-10">
+                      {tag}
+                       <LebelRemoveIcon/>
+                    </span>
+                    
+                  ))}
               </td>
             </tr>
 
