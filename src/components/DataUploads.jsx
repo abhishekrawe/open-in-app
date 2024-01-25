44e9c102e981846c7d1229301fac0ec8133be352
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-25 19:10:23 +0530

    Fixed DataUpload Issue

--- src/components/DataUploads.jsx
+++ src/components/DataUploads.jsx
@@ -1,7 +1,7 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { sampledata } from '../data/sample-data';
 import TagSlector from "../Popover/TagSlector";
-import { ChevronDownIcon, LebelRemoveIcon } from '../assets/icons'
+import { ChevronDownIcon, LebelRemoveIcon } from '../assets/icons';
 
 function DataUploads() {
   const [selectedTagsMap, setSelectedTagsMap] = useState({});
@@ -21,68 +21,46 @@ function DataUploads() {
       [dataId]: prevMap[dataId] ? [...prevMap[dataId], selectedTagValue] : [selectedTagValue],
     }));
   };
-  
+
   return (
-    <div className='rounded-3xl'>
-      <div className='md:flex items-center py-5 '>
-        <span className='text-black-10 text-2xl font-medium font-figtree leading-8'>Uploads</span>
-      </div>
-      <table className="w-full text-sm text-left bg-white-30 rounded-lg p">
-        <thead className="text-md">
-          <tr>
-            <th scope="col" className="px-6 py-6 text-gray-lightbulma ">
-              SI No.
-            </th>
-            <th scope="col" className="px-6 py-6 text-gray-lightbulma ">
-              Links
-            </th>
-            <th scope="col" className="px-6 py-6 text-gray-lightbulma ">
-              Prefix
-            </th>
-            <th scope="col" className="px-6 py-6 text-gray-lightbulma ">
-              Add Tags
-            </th>
-            <th scope="col" className="px-6 py-6 text-gray-lightbulma ">
-              Slected Tags
-            </th>
-          </tr>
-        </thead>
-        {sampledata.map((data) =>
-          <tbody key={data.id}>
-            <tr className="bg-white-20 rounded-md text-base font-figtree">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-lightbulma  whitespace-nowrap dark:text-white">
-                {data.id}
-              </th>
-              <td className="px-6 py-4 text-blue-20 underline ">
-                {data.links}
-              </td>
-              <td className="px-6 py-4 font-figtree text-gray-lightbulma ">
-                {data.prefix}
-              </td>
-              <td className="px-6 py-4 font-figtree text-gray-lightbulma ">
-                <TagSlector
-                  tags={tags}
-                  value={selectedTagsMap[data.id] || 'default'}
-                  onChange={(event) => handleTagChange(event, data.id)}
-                />
-              </td>
-              <td className="px-6 flex py-4 font-figtree text-gray-lightbulma ">
-                {selectedTagsMap[data.id] &&
-                  selectedTagsMap[data.id].map((tag, index) => (
-                    <div key={index} className="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-white-10 bg-blue-10 rounded dark:text-white-10" style={{ whiteSpace: 'nowrap' }}>
-                      {tag}<LebelRemoveIcon />
-                    </div>
-                    
-                  ))}
-              </td>
-            </tr>
+    <>
+        <div className='md:flex items-center py-5 '>
+          <div className='text-black-10 text-2xl font-medium font-figtree leading-8'>Uploads</div>
+        </div>
 
-          </tbody>
-        )}
+        <div className="overflow-x-auto">
+        <div className="flex rounded-md text-base font-figtree mb-2 p-3">
+          <div className="flex-none font-medium text-gray-lightbulma py-2 pe-12 ">SI No.  </div>
+          <div className="flex-none font-medium text-gray-lightbulma p-2  w-3/5 md:w-1/4"> Links </div>
+          <div className="flex-none font-medium text-gray-lightbulma p-2  w-3/5 md:w-1/4"> Prefixex </div>
+          <div className="flex-none font-medium text-gray-lightbulma p-2  w-3/5 md:w-1/4"> Tags </div>
+          <div className="flex-none font-medium text-gray-lightbulma p-2  w-3/5 md:w-1/6"> Selected Tags </div>
+        </div>
+        
+        {sampledata.map((data) => (
+          <div key={data.id} className="flex bg-white-20 rounded-md text-base font-figtree mb-2 p-5 w-full ">
+            <div className="flex-none font-medium text-gray-lightbulma me-4 p-2 pe-12 ">{data.id}</div>
+            <div className="flex-none text-blue-20 underline p-2 w-3/5 md:w-1/4">{data.links}</div>
+            <div className="flex-none font-figtree text-gray-lightbulma p-2 w-3/5 md:w-1/4">{data.prefix}</div>
 
-      </table>
+            <TagSlector
+              tags={tags}
+              value={selectedTagsMap[data.id] || 'default'}
+              onChange={(event) => handleTagChange(event, data.id)}
+            />
+            {selectedTagsMap[data.id] &&
+              selectedTagsMap[data.id].map((tag, index) => (
+                <div key={index} className="flex-none inline-flex items-center px-2 py-1 mx-4 text-sm font-medium text-white-10 bg-blue-10 rounded">
+                  {tag}<LebelRemoveIcon />
+                </div>
+              ))}
+          </div>
+        ))}
     </div>
-  )
+      
+    </>
+     
+  );
 }
 
 export default DataUploads;
