c37211658b3b3bfc0d724e018886a2d7afe7bb20
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-26 11:16:57 +0530

    Added the Logic to render while upload button

--- src/components/DataUploads.jsx
+++ src/components/DataUploads.jsx
@@ -7,7 +7,7 @@ function DataUploads({ data }) {
   const [selectedTagsMap, setSelectedTagsMap] = useState({});
 
   if (!data || data.length === 0) {
-    return <div>No data available</div>;
+    return <div></div>;
   }
 
   const columns = Object.keys(data[0]);
@@ -23,39 +23,47 @@ function DataUploads({ data }) {
 
   return (
     <>
-      <div className='md:flex items-center py-5 '>
-        <div className='text-black-10 text-2xl font-medium font-figtree leading-8'>Uploads</div>
-      </div>
-
-      <div className="overflow-x-auto">
-        <div className="flex rounded-md text-base font-figtree mb-2 p-3">
-          {columns.map((column, index) => (
-            <div key={index} className={`flex-none font-medium text-gray-lightbulma p-2 ${index === 0 ? 'pe-12' : ''} w-3/5 md:w-1/4`}>
-              {column}
-            </div>
-          ))}
+       <div className="p-5">
+        <div className='md:flex items-center py-5 '>
+          <div className='text-black-10 text-2xl font-medium font-figtree leading-8'>Uploads</div>
         </div>
 
-        {data.map((row, index) => (
-          <div key={index} className="flex bg-white-20 rounded-md text-base font-figtree mb-2 p-5 w-full ">
-            <div className="flex-none font-medium text-gray-lightbulma me-4 p-2 pe-12 ">{row.id}</div>
-            <div className="flex-none text-blue-20 underline p-2 w-3/5 md:w-1/4">{row.links}</div>
-            <div className="flex-none font-figtree text-gray-lightbulma p-2 w-3/5 md:w-1/4">{row.prefix}</div>
-
-            <TagSelector
-              tags={row['select tags'].split(',').map((tag) => tag.trim())}
-              value={selectedTagsMap[row.id] || 'default'}
-              onChange={(event) => handleTagChange(event, row.id)}
-            />
-            {selectedTagsMap[row.id] &&
-              selectedTagsMap[row.id].map((tag, index) => (
-                <div key={index} className="flex-none inline-flex items-center px-2 py-1 mx-4 text-sm font-medium text-white-10 bg-blue-10 rounded">
-                  {tag}<LebelRemoveIcon />
+        <div className="bg-white-30 p-4 rounded-lg">
+          <div className="overflow-x-auto">
+            <div className="flex rounded-md text-base font-figtree mb-2 p-3">
+              {columns.map((column, index) => (
+                <div key={index} className={`flex-none font-medium text-gray-lightbulma p-2 ${index === 0 ? 'pe-12' : ''} ${index === 0 ? '' : 'w-80'}`}>
+                  {column}
                 </div>
               ))}
+
+            </div>
+
+            {data.map((row, index) => (
+              <div key={index} className="flex bg-white-20 rounded-md text-base font-figtree mb-2 p-5 w-full ">
+                <div className="flex-none font-medium text-gray-lightbulma me-4 p-2 pe-12 ">{row.id}</div>
+                <div className="flex-none text-blue-20 underline p-2 w-3/5 md:w-1/4">{row.links}</div>
+                <div className="flex-none font-figtree text-gray-lightbulma p-2 w-3/5 md:w-1/4">{row.prefix}</div>
+
+                <TagSelector
+                  tags={row['select tags'].split(',').map((tag) => tag.trim())}
+                  value={selectedTagsMap[row.id] || 'default'}
+                  onChange={(event) => handleTagChange(event, row.id)}
+                />
+                {selectedTagsMap[row.id] &&
+                  selectedTagsMap[row.id].map((tag, index) => (
+                    <div key={index} className="flex-none inline-flex items-center px-2 mx-2 text-sm font-medium text-white-10 bg-blue-10 rounded">
+                      {tag}<LebelRemoveIcon />
+                    </div>
+                  ))}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+       </div>
+       
+      
+    
     </>
   );
  }
--- src/components/UploadCsv.jsx
+++ src/components/UploadCsv.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect } from "react";
-import { ExcelIcon, DocUploadIcon } from "../assets/icons";
+import React, { useRef, useState } from "react";
+import { ExcelIcon, DocUploadIcon, SpinnerIcon } from "../assets/icons";
 import Papa from 'papaparse';
 import DataUploads from "./DataUploads";
 
@@ -7,32 +7,41 @@ function UploadCsv() {
   const fileInputRef = useRef(null);
   const [data, setData] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleFileUpload = (e) => {
-    const file = e?.target?.files[0];
-    if (file) {
-      setSelectedFile(file);
-      Papa.parse(file, {
-        header: true,
-        complete: (results) => {
-          setData(results.data);
-        },
-      });
-    }
+  const handleFileUpload = (file) => {
+    setSelectedFile(file);
+    setLoading(true);
+
+    Papa.parse(file, {
+      header: true,
+      complete: (results) => {
+        setData(results.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      },
+    });
   };
-
-  const renderTags = (tags) => {
-    if (!tags) return null;
-
-    return tags.split(',').map((tag, tagIndex) => (
-      <span key={tagIndex}>{tag.trim()} </span>
-    ));
-  };
-
 
   const handleRemoveFile = () => {
     setSelectedFile(null);
     fileInputRef.current.value = null;
+    setData([]);
+  };
+
+  const handleUploadButtonClick = () => {
+    const file = fileInputRef.current.files[0];
+    if (file) {
+      handleFileUpload(file);
+    }
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
   };
 
   const handleDrop = (e) => {
@@ -49,13 +58,10 @@ function UploadCsv() {
 
   return (
     <>
-
-      {/* 1st componensts */}
       <div className='visible md:invisible md:flex items-center pb-5 '>
         <span className='text-black-10 text-2xl font-medium font-figtree leading-8'>Uploads CSV</span>
       </div>
-      <div
-        className="flex justify-center items-center" >
+      <div className="flex justify-center items-center">
         <div className="box-content h-auto w-full md:w-1/2 p-4 rounded-xl bg-white-20">
           <label
             htmlFor="fileInput"
@@ -69,7 +75,7 @@ function UploadCsv() {
               ref={fileInputRef}
               style={{ display: "none" }}
               accept=".csv"
-              onChange={(e) => handleFileUpload(e)}
+              onChange={handleFileInputChange}
             />
             <ExcelIcon />
             <h1 className="text-center">
@@ -92,21 +98,17 @@ function UploadCsv() {
             </h1>
           </label>
           <div
-            className={`justify-center gap-2 flex items-center rounded-md p-2 mt-3 text-white-10 bg-blue-10 cursor-pointer }`}
+            className={`justify-center gap-2 flex items-center rounded-md p-2 mt-3 text-white-10 bg-blue-10 cursor-pointer`}
+            onClick={handleUploadButtonClick}
           >
             <DocUploadIcon />
             Upload
+            {loading && <SpinnerIcon />}
           </div>
         </div>
-
       </div>
-
-
-      {/* <2nd component /> */}
-
       <DataUploads data={data} />
     </>
-
   );
 }
 
