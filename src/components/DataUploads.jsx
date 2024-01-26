46d6aa9b65a4bab2f8938e7fad092d20bbec9dc3
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-27 02:13:00 +0530

    Fixed all the build issue

--- src/components/DataUploads.jsx
+++ src/components/DataUploads.jsx
@@ -79,8 +79,8 @@ function DataUploads({ data }) {
                   selectedTagsMap[row.id].map((tag, index) => (
                     <div key={index} className="flex-none inline-flex items-center px-2 mx-2 text-sm font-medium text-white-10 bg-blue-10 my-3 rounded">
                       {tag}
-                      <button type="button" class="inline-flex items-center p-1 ms-2 text-sm text-pink-400 bg-transparent rounded-sm hover:bg-pink-200 hover:text-pink-900 dark:hover:bg-pink-800 dark:hover:text-pink-300" data-dismiss-target="#badge-dismiss-pink" onClick={() => handleBadgeDismiss(row.id, index)} aria-label="Remove">
-                        <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <button type="button" className="inline-flex items-center p-1 ms-2 text-sm text-pink-400 bg-transparent rounded-sm hover:bg-pink-200 hover:text-pink-900 dark:hover:bg-pink-800 dark:hover:text-pink-300" data-dismiss-target="#badge-dismiss-pink" onClick={() => handleBadgeDismiss(row.id, index)} aria-label="Remove">
+                        <svg className="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                         </svg>
                       </button>
